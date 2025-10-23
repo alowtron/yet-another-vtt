ce2a1a8abec1bac1d47606d087388ab728b55314
alowtron <alowtron@gmail.com>
2025-10-23 14:23:01 -0400

    added more active skills for yet another ttrpg

--- components/creatures/creatureSheets/addSkill.tsx
+++ components/creatures/creatureSheets/addSkill.tsx
@@ -18,7 +18,47 @@ const skills = [
   {
     name: 'Coat With Fast Acting Poison',
     actions: 1,
-    effect: 'coat target weapon or projectile with poison, next attack made with it in the next minute deals an extra 1d6 damage'
+    effect: 'coat target weapon or projectile with poison, next attack made with it in the next minute deals an extra 1d6 damage.'
+  },
+  {
+    name: 'Coat With Healing Mitigation Poison',
+    actions: 1,
+    effect: 'Coat the weapon or projectile in a poison, the next attack made with it in the next minute makes it so that the person hit by it can not heal or be healed for one day.'
+  },
+  {
+    name: 'Coat With Slow Acting Poison',
+    actions: 1,
+    effect: 'Coat target weapon or projectile with poison, next attack made with it in the next minute makes the target take 1d6 damage at the end of every hour for 8 hours.'
+  },
+  {
+    name: 'Piercing Strike',
+    actions: 1,
+    effect: 'Choose a creature and your next attack against the target creature ignores armor.'
+  },
+  {
+    name: 'Critical Strike',
+    actions: 1,
+    effect: 'Choose a creature and line up the perfect attack, your next attack against the chosen creature deals twice as much damage.'
+  },
+  {
+    name: 'Taunt',
+    actions: 1,
+    effect: 'Make a Charisma or Intellect check against a target creature\'s Intellect check, if the other creature fails, they can only attack you next turn.'
+  },
+  {
+    name: 'Frighten',
+    actions: 1,
+    effect: 'Make a Strength, Intellect, or Charisma check to scare other creatures of your choice that can hear you. They make an intellect check, on a fail, any damage they do to a creature other than you is halved (rounded down).'
+  },
+  {
+    name: 'Grapple',
+    actions: 1,
+    effect: 'Make a Strength check vs target Strength check. If you win, the target is grappled and can no longer move or take actions, except to try to escape. While you have someone grappled you can drag them with you wherever you move. To escape they can use an action to make a Strength check vs you.'
+  },
+  {
+    name: 'Spin Attack',
+    actions: 1,
+    effect: 'Attack all creatures of your choice within 5 feet of you dealing 1d6 damage.'
   },
 ]
 
@@ -58,8 +98,6 @@ export default function AddSkill({
           )}
         </div>
       ))}
-
-      
     </div>
   )
 }
