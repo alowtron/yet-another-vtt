478ee9c179071a1a25af615832516f36ee1a9b4e
alowtron <alowtron@gmail.com>
2025-10-26 16:49:38 -0400

    added some jsdoc for the DisplayStats

--- components/creatures/creatureSheets/displayStats.tsx
+++ components/creatures/creatureSheets/displayStats.tsx
@@ -7,6 +7,16 @@ interface DisplayStatsProps {
   onUpdate: (index: number, statInfo: any) => void
 }
 
+/**
+ * Displays the stats for ttrpg
+ * @param {Array<{
+ *  name: string,
+ *  modifier? number,
+ *  stat?: number
+ * }>} stats, takes the name of the stat, then have a couple of optional values that it shows, and can be updated with the onUpdate pass in
+ * @param {() => void} onUpdate, pass in a stat you want to be able to change, for dnd this would be the stat, not the modifier and the modifier is calculated off the stat.
+ * @returns a div of html displaying anything along with inputs to change stuff.
+ */
 export default function DisplayStats({
   stats,
   onUpdate
