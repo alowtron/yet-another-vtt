3fa001d25a5c29d9e489b76732b2e01709165aca
alowtron <alowtron@gmail.com>
2025-11-10 09:56:15 -0500

    added a form t o add a custom item

--- components/creatures/creatureSheets/inventory.tsx
+++ components/creatures/creatureSheets/inventory.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface DisplayInventoryProps {
   inventory: Array<{
     name: string,
@@ -8,17 +10,39 @@ interface DisplayInventoryProps {
 export default function DisplayInventory({
   inventory,
 }: DisplayInventoryProps) {
-  
+  const [showAddCustom, setShowAddCustom] = useState(false)
+
+  async function addCustomItem(formData: FormData) {
+    const name = formData.get('name')
+    const weight = formData.get('weight')
+    console.log('formssss' + name + weight)
+    setShowAddCustom(false)
+  }
   
   return (
     <div>
       {inventory.map(({name}: {name: string}, index: number) => (
-            <div>
-              <div>
-                <b>{name}:</b> {inventory[index].weight} pounds
-              </div>
-            </div>
-          ))}
+        <div key={index}>
+          <div>
+            <b>{name}:</b> {inventory[index].weight} pounds
+          </div>
+        </div>
+      ))}
+      {showAddCustom ? (
+        <div>
+          <form action={addCustomItem}>
+            Name:<input name="name"></input>
+            <br></br>
+            Weight:<input name="weight" type="number"></input>
+            <br></br>
+            <button type='submit'>Add Item</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => setShowAddCustom(true)}>Add Custom Item</button>
+        </div>
+      )}
     </div>
   )
 }
