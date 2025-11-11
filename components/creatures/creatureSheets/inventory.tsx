2cba23a6e545802bc15d1582d9df56a0776b3fa6
alowtron <alowtron@gmail.com>
2025-11-11 13:50:19 -0500

    you can now cancel adding a item and fixed the inventory add error for the form

--- components/creatures/creatureSheets/inventory.tsx
+++ components/creatures/creatureSheets/inventory.tsx
@@ -36,17 +36,23 @@ export default function DisplayInventory({
       ))}
       {showAddCustom ? (
         <div>
+          <hr></hr>
+          <label htmlFor="newItem">
+            Add Custom Item
+          </label>
           <form action={addCustomItem}>
-            Name:<input name="name" type="text"></input>
+            Name:<input id="newItem" name="name" type="text"></input>
             <br></br>
-            Weight:<input name="weight" type="number"></input>
+            Weight:<input id="newItem" name="weight" type="number"></input>
             <br></br>
             <button type='submit'>Add Item</button>
+            <button onClick={() => setShowAddCustom(false)}>Cancel</button>
           </form>
         </div>
       ) : (
         <div>
           <button onClick={() => setShowAddCustom(true)}>Add Custom Item</button>
+          
         </div>
       )}
     </div>
