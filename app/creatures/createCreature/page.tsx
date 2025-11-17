1cb681af5f4f943d474c99b50567b9ec0cd252d3
alowtron <alowtron@gmail.com>
2025-11-17 12:44:54 -0500

    createCreature is starting to work properly now

---
+++ app/creatures/createCreature/page.tsx
@@ -0,0 +1,176 @@
+'use client'
+
+import { useAuth } from "@clerk/clerk-react"
+import { useState } from "react"
+import { useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { useRouter } from "next/navigation"
+
+const creatureTypeToSelect = [
+  {
+    name: 'YetAnotherTTRPG',
+    value: 'yet_another_ttrpg'
+  },
+  {
+    name: 'example, do not click',
+    value: ''
+  }
+]
+
+export default function CreateCreature() {
+  const [creatureType, setCreatureType] = useState('')
+  const [creationPart, setCreationState] = useState('')
+
+  const addCreature = useMutation(api.creatures.addCreature)
+  const { userId } = useAuth()
+  const router = useRouter()
+
+  function addCreatureFunction(creatureInfo = {}, creatureName = 'new creature') {
+    if (userId) {
+      addCreature({
+        userId: userId,
+        creatureType: "yet_another_ttrpg",
+        creatureName: creatureName,
+        creatureInfo: creatureInfo
+      })
+    }
+    router.push('../creatures')
+  }
+
+  function createCreature(type: string) {
+    let creationInfo = {
+      maxHP: 10,
+      currentHP: 10,
+      race: 'null',
+      speed: 20,
+      level: 1,
+      physicalDR: 0,
+      magicalDR: 0,
+      skills: [],
+      passiveSkills: [],
+      inventory: [
+        {
+          name: 'backpack',
+          weight: 1
+        },
+        {
+          name: 'bread',
+          weight: 0.1
+        }
+      ],
+      stats: [
+        {
+          name: 'physical',
+          modifier: 3
+        },
+        {
+          name: 'fortitude',
+          modifier: 3
+        },
+        {
+          name: 'intellect',
+          modifier: 3
+        },
+        {
+          name: 'charisma',
+          modifier: 3
+        }
+      ]
+    }
+
+    if (type == 'tank') {
+      creationInfo.maxHP = 12
+      creationInfo.race = 'dwarf'
+    } else if (type == 'fighter') {
+
+    }
+
+    addCreatureFunction(creationInfo, 'tank')
+  }
+
+  return (
+    <div>
+      Create A creature
+      {creatureType == 'yet_another_ttrpg' ? (
+        <div>
+          {creationPart == '' ? (
+            <div>
+              <button onClick={() => addCreatureFunction()}>
+                Empty Character
+              </button>
+              <br></br>
+              <button onClick={() => setCreationState('quick')}>
+                Quick Character Creation
+              </button>
+            </div>
+          ) : creationPart == 'quick' ? (
+            <div>
+              <h2>
+                Fighting Style?
+              </h2>
+              <button onClick={() => setCreationState('weapons')}>
+                Weapons
+              </button>
+              <button>
+                Magic
+              </button>
+            </div>
+          ): creationPart == 'weapons' ? (
+            <div>
+              <h2>
+                Fighting Style?
+              </h2>
+              <button onClick={() => setCreationState('melee')}>
+                Melee
+              </button>
+              <button onClick={() => setCreationState('ranged')}>
+                Ranged
+              </button>
+            </div>
+          ) : creationPart == 'melee' ? (
+            <div>
+              <h2>
+                Melee Fighting Style
+              </h2>
+              <button onClick={() => createCreature('tank')}>
+                Tank
+              </button>
+              <button>
+                Fighter
+              </button>
+            </div>
+          ) : creationPart == 'ranged' ? (
+            <div>
+              <h2>
+                Ranged Fighting Style
+              </h2>
+              <button>
+                Ranger
+              </button>
+              <button>
+                Rogue
+              </button>
+            </div>
+          ) : (
+            <div>
+              this should not be showing
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          <h3>
+            Select the system and type of creature you want to create
+          </h3>
+          {creatureTypeToSelect.map(({name, value}: {name: string, value: string}, index) => (
+            <div key={index}>
+              <button onClick={() => setCreatureType(value)}>
+                {name}
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
--- app/creatures/page.tsx
+++ app/creatures/page.tsx
@@ -3,27 +3,19 @@
 import { useMutation } from "convex/react"
 import { api } from "@/convex/_generated/api"
 import { useAuth } from "@clerk/clerk-react"
+import { useRouter } from "next/navigation"
 import ListCreatures from "@/components/creatures/listCreatures"
 
 export default function Creatures() {
   const addCreature = useMutation(api.creatures.addCreature)
   const { userId } = useAuth()
+  const router = useRouter()
 
-  function addCreatureFunction() {
-    if (userId) {
-      addCreature({
-        userId: userId,
-        creatureType: "yet_another_ttrpg",
-        creatureName: 'temp',
-        creatureInfo: {}
-      })
-    }
-    
-  }
+  
   return (
     <div>
       Creature page
-      <button onClick={() => addCreatureFunction()}>Add creature</button>
+      <button onClick={() => router.push('./creatures/createCreature')}>Add creature</button>
       <ListCreatures>
         
       </ListCreatures>
